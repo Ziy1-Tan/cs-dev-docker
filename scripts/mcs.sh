ccb3c85adc3ad5762160b0476bc172ab082e6944
qggcs <ajb459684460@gmail.com>
2022-06-06 15:04:55 +0800

    add status check

--- scripts/mcs.sh
+++ scripts/mcs.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+color_normal=$(tput sgr0)
+color_bold=$(tput bold)
+color_red="$color_bold$(tput setaf 1)"
+color_green="$color_bold$(tput setaf 2)"
+
+check_service()
+{
+  if systemctl is-active --quiet $1; then
+      echo "$1 service $color_green OK $color_normal"
+  else
+      echo "$1 service $color_red failed $color_normal"
+      service $1 status
+  fi
+}
+
 case $1 in
     s|start)
         echo "Starting..."
         systemctl start mariadb-columnstore.service
+        check_service mariadb-columnstore
         systemctl start mariadb.service
+        check_service mariadb
         echo "Start finished..."
         ;;
     p|stop)
@@ -16,7 +33,9 @@ case $1 in
     r|restart)
         echo "Restarting..."
         systemctl restart mariadb-columnstore.service
+        check_service mariadb-columnstore
         systemctl restart mariadb.service
+        check_service mariadb
         echo "Restarting finished..."
         ;;
     *)
