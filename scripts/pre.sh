39eb0a829e5053849caf8d24f45a74afe3405eb2
livia.hj&champ <ajb459684460@gmail.com>
2022-06-15 22:05:14 +0800

    Update pre.sh

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -2,5 +2,3 @@
 
 mkdir /run/mysqld && chown -R mysql:mysql /run/mysqld
 mkdir /run/mariadb && chown -R mysql:mysql /run/mariadb
-
-ExecStartPre=/bin/sh -c "mkdir -p /run/mysqld; chown -R mysql:mysql /run/mysqld; mkdir -p /run/mariadb; chown -R mysql:mysql /run/mariadb"
